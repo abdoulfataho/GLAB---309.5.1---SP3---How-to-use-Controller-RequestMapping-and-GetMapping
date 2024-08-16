f5c145f703b5f5903127d7266bdcdaf4c3b772d5
abdoulfataho <yamyaogofataho@gmail.com>
2024-08-16 10:42:09 -0400

    GLAB 309.5.3 - SP5 - How to use - Model, ModelAndView, and @ModelAttribute annotation

---
+++ ExerciseOneRest/src/main/java/com/example/ExerciseOneRest/controller/ModelAndViewcontroller.java
@@ -0,0 +1,31 @@
+package com.example.ExerciseOneRest.controller;
+
+import ch.qos.logback.core.model.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Arrays;
+import java.util.List;
+@Controller
+public class ModelAndViewcontroller {
+//    @GetMapping("/showViewPage")
+//    public String passParametersWithModel(Model model) {
+//        model.addAttribute("message", "Perscholas");
+//        model.addAttribute("welcomeMessage", "welcome");
+//        int i = 10;
+//        model.addAttribute("number", i);
+//
+//        List<String> list = Arrays.asList("one", "two");
+//        model.addAttribute("listData", list);
+//        return "viewPage";
+//    }
+    @GetMapping("/goToViewPage")
+    public ModelAndView passParametersWithModelAndView() {
+        String st = "Welcome to the Page";
+        ModelAndView modelAndView = new ModelAndView("viewPage_2");
+        modelAndView.addObject("message", st);
+        modelAndView.addObject("info", "Employee information.");
+        return modelAndView;
+    }
+}
